c88858f6ae49dca4b1ac3e1c2441b6d559c02e6f
Ruslan <41823506+RuslanPylypenko@users.noreply.github.com>
2021-10-09 12:33:36 +0300

    Added users crud

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,16 @@ Route::get('/', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/verify/{token}', 'Auth\RegisterController@verify')->name('register.verify');
+
+Route::group(
+    [
+        'prefix' => 'admin',
+        'as' => 'admin.',
+        'namespace' => 'Admin',
+        'middleware' => ['auth'],
+    ],
+    function () {
+        Route::get('/', 'HomeController@index')->name('home');
+        Route::resource('users', 'UsersController');
+    }
+);
