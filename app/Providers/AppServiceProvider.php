58e97b976345559a69b30ad09e533823790c4888
Ruslan <41823506+RuslanPylypenko@users.noreply.github.com>
2021-10-09 11:33:56 +0300

    removed unused code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,11 +6,7 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
+
     public function register()
     {
         if ($this->app->isLocal()) {
@@ -18,11 +14,7 @@ class AppServiceProvider extends ServiceProvider
         }
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
+
     public function boot()
     {
         //
